f3c31304f8077c0131f898943d9d02b8836d6742
vivekmehto <vivekmehto.webdev@gmail.com>
2022-01-11 20:10:19 +0530

    ques 3 and 4

---
+++ numbers/ques3.js
@@ -0,0 +1,5 @@
+//Write a program to calculate the kinetic energy of a body with mass 'm' and volume 'v'.
+
+const kineticEnergy = (mass, volume) => 0.5 * mass * volume * volume;
+
+kineticEnergy(2, 2);
---
+++ numbers/ques4.js
@@ -0,0 +1,6 @@
+//Write a program to convert Fahrenheit to Ce
+// For Fahrenheit to Celsius conversion use: C = (F - 32) * 5/9 'F' and 'C' are the temperatures on the Fahrenheit scale and Celsius scale respectively
+
+const temperatureConvert = (f) => ((f - 32) * 5) / 9;
+
+temperatureConvert(56);
