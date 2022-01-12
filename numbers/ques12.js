a5d91acdf590a06acdc31f0eb4d222658cff8cc3
vivekmehto <vivekmehto.webdev@gmail.com>
2022-01-12 21:59:46 +0530

    Complete Numbers

--- numbers/ques12.js
+++ numbers/ques12.js
@@ -2,12 +2,29 @@
 
 const decimalToBinary = (num) => {
   let binary = 0;
+  let i = 1;
   while (num > 0) {
     let rem = num % 2;
-    num = num - 2;
-    binary = binary * 10 + rem;
+    num = parseInt(num / 2);
+    binary = binary + rem * i;
+    i = i * 10;
   }
   return binary;
 };
 
 //Follow up Question : Write a Program to Convert Octal to Decimal.
+
+const octalToDecimal = (num) => {
+  let decimalNumber = 0;
+  let i = 0;
+  while (num > 0) {
+    let rem = num % 10;
+    decimalNumber = decimalNumber + rem * Math.pow(8, i);
+    num = parseInt(num / 10);
+    i++;
+  }
+  return decimalNumber;
+};
+
+decimalToBinary(5);
+octalToDecimal(116);
