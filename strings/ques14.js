976886a37668c61117fc9aaf0778c1e8eebc8e2b
vivekmehto <vivekmehto.webdev@gmail.com>
2022-01-16 22:10:59 +0530

    Update ques14.js

--- strings/ques14.js
+++ strings/ques14.js
@@ -1,1 +1,12 @@
 //Write a program that masks all but last four characters of the string "5565534276455423" to '#'
+
+const maskStr = (str) => {
+  let resultStr = "";
+  for (let i = 0; i < str.length - 4; i++) {
+    resultStr += str[i];
+  }
+  resultStr += "#";
+  return resultStr;
+};
+
+maskStr("5565534276455423");
