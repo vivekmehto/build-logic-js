573261f8276d31984b2292f1d5d90688c147ba43
vivekmehto <vivekmehto.webdev@gmail.com>
2022-01-16 22:06:34 +0530

    Update ques13.js

--- strings/ques13.js
+++ strings/ques13.js
@@ -1,1 +1,14 @@
 //Write a program that removes the time from the given date string "Wed April 15, 7pm". It should return only the date without the time.
+
+const removeTime = (str) => {
+  let resultStr = "";
+  for (let i = 0; i < str.length; i++) {
+    if (str[i] === ",") {
+      break;
+    }
+    resultStr += str[i];
+  }
+  return resultStr;
+};
+
+removeTime("Wed April 15,7pm");
