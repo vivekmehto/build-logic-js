964a62943eac13778e63986d50abf77484a8f25c
vivekmehto <vivekmehto.webdev@gmail.com>
2022-01-16 22:21:23 +0530

    Update ques15.js

--- strings/ques15.js
+++ strings/ques15.js
@@ -1,1 +1,24 @@
 //Given a string "tic tac toe is a fun game" convert the first 6 characters to capital case
+
+const capitalCase = (char) => String.fromCharCode(char.charCodeAt(0) - 32);
+
+const firstSixCapital = (str) => {
+  let resultStr = "";
+  let count = 0;
+  for (let i = 0; i < str.length; i++) {
+    if (count !== 6) {
+      if (str[i] === " ") {
+        resultStr += str[i];
+      } else {
+        let capitalChar = capitalCase(str[i]);
+        resultStr += capitalChar;
+        count++;
+      }
+    } else {
+      resultStr += str[i];
+    }
+  }
+  return resultStr;
+};
+
+firstSixCapital("tic tac toe is a fun game");
