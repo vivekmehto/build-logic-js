8984dfff35ab7224031fa28471bda5257fbe92f0
vivekmehto <vivekmehto.webdev@gmail.com>
2022-01-23 22:32:38 +0530

    Create ques1.js

---
+++ object-oops/ques1.js
@@ -0,0 +1,14 @@
+/*
+Given an array of objects of student's marks:
+
+Print the name and total marks of each student.
+Print the name of student whose total marks is highest.
+Print the name of student whose total marks is lowest.
+Print the average marks of the class in computer subject.
+Print the grades of all students:
+(Grade A if total marks is higher than or equal to 75%,
+Grade B if higher than or equal to 60%,
+Grade C if higher than or equal to 35%,
+Grade D if lower than 35%.)
+Print the total number of students passed and their names. Pass when total marks is greater than 35%.
+*/
