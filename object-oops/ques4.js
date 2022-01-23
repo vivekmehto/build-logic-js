cb423e3b5723a2e5cdf9f20701c4bfb06b5c8804
vivekmehto <vivekmehto.webdev@gmail.com>
2022-01-23 22:34:55 +0530

    Create ques4.js

---
+++ object-oops/ques4.js
@@ -0,0 +1,8 @@
+/**
+ * Given an array of objects of items in cart, print:
+
+the total No. of items
+the total cart value
+the total discounted value(sum of dicounted values on each item) based on the given discount
+total tax amount (18% tax, calculated on total cart value)
+ */
