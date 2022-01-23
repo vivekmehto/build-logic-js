ba7c8643a9e27186eaca2dfa25ff53c0ef0f32d9
vivekmehto <vivekmehto.webdev@gmail.com>
2022-01-23 22:33:39 +0530

    Create ques2.js

---
+++ object-oops/ques2.js
@@ -0,0 +1,8 @@
+/**
+ * Salary calculation using OOPS concept.
+
+Create a Class using ES6 in JavaScript named Employee and assign necessary
+data members and methods such as name, id, basic salary, HRA, Allowances; define getSalary method which will return the net salary.
+Create two Instances of Employee with all necessary details.
+Call the getSalary method of each instance and return the net salary based on your computation.
+ */
